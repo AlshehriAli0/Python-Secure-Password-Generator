b630c4b4a3a6f656b3b072515b48d54b6424ff62
Ali Alshehri <Ali0fawzish@outlook.com>
2023-11-17 17:49:47 +0300

    first commit

---
+++ passwordGenerator.py
@@ -0,0 +1,27 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the super secure password Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+lr = []
+
+total = nr_numbers + nr_letters + nr_symbols
+
+for i in range(0,nr_letters):
+   lr.append(random.choice(letters))
+for i in range(0,nr_symbols):
+   lr.append(random.choice(symbols))
+for i in range(0,nr_numbers):
+   lr.append(random.choice(numbers))
+nl = []
+for i in range(0, total):
+    c = random.choice(lr)
+    nl.append(c)
+    lr.remove(c)
+
+print(''.join(nl))
